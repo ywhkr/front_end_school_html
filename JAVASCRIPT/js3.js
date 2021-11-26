dfef8beed57ba6b871331ee152aabf51d1302eb4
ywhkr <69128735+ywhkr@users.noreply.github.com>
2021-11-26 23:53:26 +0900

    20일차 자바스크립트 시작

---
+++ JAVASCRIPT/js3.js
@@ -0,0 +1,25 @@
+var 변수하나 = 20;
+var 변수둘 = 10;
+var 변수셋 = 2;
+
+document.write("변수하나 : ", 변수하나);
+document.write("<br>");
+document.write("변수둘 : ", 변수둘);
+document.write("<br>");
+document.write("변수셋 : ", 변수셋);
+document.write("<br>");
+document.write("변수하나+변수둘 : ", 변수하나 + 변수둘);
+document.write("<br>");
+document.write("변수하나-변수둘 : ", 변수하나 - 변수둘);
+document.write("<br>");
+document.write("변수하나/변수둘 : ", 변수하나 / 변수둘);
+document.write("<br>");
+document.write("변수하나*변수둘 : ", 변수하나 * 변수둘);
+document.write("<br>");
+document.write("변수하나**변수셋 : ", 변수하나 ** 변수셋);
+document.write("<br>");
+document.write("변수하나%변수셋 : ", 변수하나 % 변수셋);
+
+// **은 제곱 의미
+
+let number = parseFloat("13.3px.10", 10);
